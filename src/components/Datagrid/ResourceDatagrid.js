8f7ba717dee45b3846ec4052de6819acb0438c5d
nidavids <nidavids@cisco.com>
2022-07-29 14:38:02 -0700

    Added CRUD functions
    - Added functions that allow for resource creation, deletion, and updating.

--- src/components/Datagrid/ResourceDatagrid.js
+++ src/components/Datagrid/ResourceDatagrid.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SecurityIcon from '@mui/icons-material/Security';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
 import axios from 'axios';
+
+
 
 const columns = [
     { field: 'type', headerName: 'Resource Type', width: 150, editable: true, },
@@ -28,13 +33,86 @@ const columns = [
     },
 ];
 
-
-
-
 export default function ResourceDatagrid() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [rows, setRows] = useState(null);
+
+    const deleteRow = React.useCallback(async (params) => {
+        const { rowData } = params;
+        const { id } = rowData;
+        const url = `http://localhost:8080/api/v1/resources/${id}`;
+        const response = await axios.delete(url);
+        if (response.status === 200) {
+            const newData = data.filter(row => row.id !== id);
+            setData(newData);
+        }
+    }
+        , [data]);
+
+    const toggleStatus = React.useCallback(async (params) => {
+        const { rowData } = params;
+        const { id } = rowData;
+        const url = `http://localhost:8080/api/v1/resources/${id}/status`;
+        const response = await axios.put(url);
+        if (response.status === 200) {
+            const newData = data.map(row => {
+                if (row.id === id) {
+                    row.status.state = row.status.state === 'active' ? 'inactive' : 'active';
+                }
+                return row;
+            }
+            );
+            setData(newData);
+        }
+    }
+        , [data]);
+    
+    const createRow = React.useCallback(async (params) => {
+        const { rowData } = params;
+        const { id } = rowData;
+        const url = `http://localhost:8080/api/v1/resources/${id}/status`;
+        const response = await axios.put(url);
+        if (response.status === 200) {
+            const newRow = {
+                id: response.data.id,
+                type: response.data.type,
+                status: {
+                    state: response.data.status.state,
+                    fault: response.data.status.fault,
+                    createdTime: response.data.status.createdTime,
+                    lease: response.data.status.lease,
+                }
+            };
+            const newData = [...data, newRow];
+            setData(newData);
+        }
+    }
+        , [data]);
+
+    const updateRow = React.useCallback(async (params) => {
+        const { rowData } = params;
+        const { id } = rowData;
+        const url = `http://localhost:8080/api/v1/resources/${id}`;
+        const response = await axios.put(url);
+        if (response.status === 200) {
+            const newData = data.map(row => {
+                if (row.id === id) {
+                    row.type = response.data.type;
+                    row.status.state = response.data.status.state;
+                    row.status.fault = response.data.status.fault;
+                    row.status.createdTime = response.data.status.createdTime;
+                    row.status.lease = response.data.status.lease;
+                }
+                return row;
+            }
+            );
+            setData(newData);
+        }
+    }
+        , [data]);
+
 
     const getData = async () => {
         try {
