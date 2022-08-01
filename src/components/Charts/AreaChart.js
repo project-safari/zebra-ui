cefd8ecdad16dad3b2ac659483d7458d9fbec3df
nidavids <nidavids@cisco.com>
2022-08-01 11:59:25 -0700

    Fixes #10
    - Adds an analytic view
    - Adds CRUD, needs API methods
    - Deleted unnecessary columns, added more relevant ones.
    - Green Checkbox for active State, did not change icon for type looked wrong and hard to tell without key. Will look into different ways of showing this.

--- src/components/Charts/AreaChart.js
+++ src/components/Charts/AreaChart.js
@@ -3,45 +3,45 @@ import React from "react";
 
 const data = [
     {
-      "name": "Page A",
-      "uv": 4000,
-      "pv": 2400,
+      "name": "07/31",
+      "Available": 4000,
+      "Leased": 2400,
       "amt": 2400
     },
     {
-      "name": "Page B",
-      "uv": 3000,
-      "pv": 1398,
+      "name": "08/01",
+      "Available": 3000,
+      "Leased": 1398,
       "amt": 2210
     },
     {
-      "name": "Page C",
-      "uv": 2000,
-      "pv": 9800,
+      "name": "08/02",
+      "Available": 2000,
+      "Leased": 9800,
       "amt": 2290
     },
     {
-      "name": "Page D",
-      "uv": 2780,
-      "pv": 3908,
+      "name": "08/03",
+      "Available": 2780,
+      "Leased": 3908,
       "amt": 2000
     },
     {
-      "name": "Page E",
-      "uv": 1890,
-      "pv": 4800,
+      "name": "08/04",
+      "Available": 1890,
+      "Leased": 4800,
       "amt": 2181
     },
     {
-      "name": "Page F",
-      "uv": 2390,
-      "pv": 3800,
+      "name": "08/05",
+      "Available": 2390,
+      "Leased": 3800,
       "amt": 2500
     },
     {
-      "name": "Page G",
-      "uv": 3490,
-      "pv": 4300,
+      "name": "08/06",
+      "Available": 3490,
+      "Leased": 4300,
       "amt": 2100
     }
   ]
@@ -51,11 +51,11 @@ export default function ResourceAreaChart () {
         <AreaChart width={650} height={250} data={data}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="colorAvailable" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
                 <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
             </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="colorLeased" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
                 <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
             </linearGradient>
@@ -64,8 +64,8 @@ export default function ResourceAreaChart () {
             <YAxis />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            <Area type="monotone" dataKey="Available" stroke="#8884d8" fillOpacity={1} fill="url(#colorAvailable)" />
+            <Area type="monotone" dataKey="Leased" stroke="#82ca9d" fillOpacity={1} fill="url(#colorLeased)" />
         </AreaChart>
     );
 }
