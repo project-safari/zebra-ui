5180f120b5f381758621e3e22d7f4cf1e9307f04
nidavids <nidavids@cisco.com>
2022-07-31 21:53:07 -0700

    Removed unused files
    - Removed unused api requests
    - Added service validation for registration
    - Added helper functions to get and set items in local storage

--- src/contracts/resource-get-config/resource-get-config.api.ts
+++
@@ -1,9 +0,0 @@
-import {
-    service as inventoryService
-}
-
-const getInventory = (params: DefaultApiInventoryGetRequest) => inventoryService.DefaultApiEnrollInventoryRequest(params)
-    .then(r => r.data)
-    .catch(e => e);
-
-export default {getInventory}
